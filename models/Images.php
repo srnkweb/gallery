81659ebd4e94b7354af1e48d417ffc4f18da63e4
srnkweb <srnkweb@gmail.com>
2016-10-03 13:08:45 +0300

    исправлены пространства имен -> core/DB.php, именено обращение к методам со статического на объектное -> models/Images.php

--- models/Images.php
+++ models/Images.php
@@ -1,19 +1,19 @@
 <?php
 namespace gallery\models;
+use gallery\core;
 //require_once __DIR__ . "/../core/DB.php";
 //require_once __DIR__ . "/AbstractModel.php";
 
-class Images extends AbstractModel
+class Images
 {
 	public static function getImagesById()
 	{
 		$sql = "SELECT id, url, alt  FROM images WHERE id=:img";
 		$binParam = [[':img' => $_GET['img']]];
-		//$class = 'Images';
 		$class = __CLASS__;
-
-		parent::connectDB();
-		return parent::query($sql, $class, $binParam);
+		$dbh = new core\DB();
+		$dbh->connectDB();
+		return $dbh->query($sql, $class, $binParam);
 	}
 }
 
