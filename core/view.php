183749c5b7048996d78133b9012dec6b6f780451
srnkweb <srnkweb@gmail.com>
2016-11-06 22:46:41 +0300

    Удалены не нужные комментарии

--- core/view.php
+++ core/view.php
@@ -11,24 +11,12 @@ class view
 	{
 		$paginator = $data[0];
 		$image = $data[1];
-		// $header_view = view::getHeaderView($this->header_view);
-		/*if(is_array($data)) {
-
-			// преобразуем элементы массива в переменные
-
-		}*/
 
 		$paginator = classes\TransformObj::objectToArrayKeyValue($data[0]);
-		var_dump($paginator);
 		$image = classes\TransformObj::objectToArrayKeyValue($data[1]);
 
 		$regFormView = 'authentication.php';
 		$gallery = $content_view;
-		/*
-		динамически подключаем общий шаблон (вид),
-		внутри которого будет встраиваться вид
-		для отображения контента конкретной страницы.
-		*/
 		include __DIR__ . "/../template/$template_view";
 	}
 }
